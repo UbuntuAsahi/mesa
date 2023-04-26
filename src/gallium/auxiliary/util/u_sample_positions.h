0bbca9a26fb426774b8da79e230fb8af8dc39e1c
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2023-04-26 14:48:08 +0000

    asahi: Use u_default_get_sample_position

---
+++ src/gallium/auxiliary/util/u_sample_positions.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2023 Alyssa Rosenzweig
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef U_SAMPLE_POSITIONS_H
+#define U_SAMPLE_POSITIONS_H
+
+#include "pipe/p_context.h"
+
+void
+u_default_get_sample_position(struct pipe_context *ctx,
+                              unsigned sample_count,
+                              unsigned sample_index,
+                              float *out_value);
+
+#endif
