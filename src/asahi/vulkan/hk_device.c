4dbf50e3d97fd7f807f145e0e6466acf115c4b01
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2024-09-28 20:50:40 -0400

    hk: add norobust test
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- src/asahi/vulkan/hk_device.c
+++ src/asahi/vulkan/hk_device.c
@@ -40,6 +40,7 @@ static const struct debug_named_value hk_perf_options[] = {
    {"noborder",  HK_PERF_NOBORDER, "Disable custom border colour emulation"},
    {"nobarrier", HK_PERF_NOBARRIER,"Ignore pipeline barriers"},
    {"batch",     HK_PERF_BATCH,    "Batch submissions"},
+   {"norobust",  HK_PERF_NOROBUST, "Disable robustness"},
    DEBUG_NAMED_VALUE_END
 };
 /* clang-format on */
@@ -345,6 +346,14 @@ hk_CreateDevice(VkPhysicalDevice physicalDevice,
 
    dev->vk.shader_ops = &hk_device_shader_ops;
    dev->vk.command_dispatch_table = &dev->cmd_dispatch;
+
+   if (HK_PERF(dev, NOROBUST)) {
+      dev->vk.enabled_features.robustBufferAccess = false;
+      dev->vk.enabled_features.robustBufferAccess2 = false;
+      dev->vk.enabled_features.robustImageAccess = false;
+      dev->vk.enabled_features.robustImageAccess2 = false;
+      dev->vk.enabled_features.pipelineRobustness = false;
+   }
 
    drmDevicePtr drm_device = NULL;
    int ret = drmGetDeviceFromDevId(pdev->render_dev, 0, &drm_device);
